a5c52f2f777ada731caed2aade28baf79b1b07e6
vlopeza9 <vlopeza9@miumg.edu.gt>
2023-06-09 01:55:41 -0600

    Commit

--- src/main/java/controller/ResponsableController/IndexController.java
+++ src/main/java/controller/ResponsableController/IndexController.java
@@ -6,7 +6,11 @@ package controller.ResponsableController;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 /**
  * FXML Controller class
@@ -14,6 +18,24 @@ import javafx.fxml.Initializable;
  * @author usuario
  */
 public class IndexController implements Initializable {
+
+    @FXML
+    public TableColumn<?, ?> acciones;
+
+    @FXML
+    public Button botoncrearresponsable;
+
+    @FXML
+    public TableColumn<?, ?> id;
+
+    @FXML
+    public TableColumn<?, ?> nombre;
+
+    @FXML
+    public TableView<?> tablaResponsables;
+
+    @FXML
+    public TableColumn<?, ?> tipo;
 
     /**
      * Initializes the controller class.
